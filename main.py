770587543126bd029fc9f887bbe4701d49059aa9
V Thulisile Sibanda <66913810+thulieblack@users.noreply.github.com>
2022-11-11 16:51:06 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from typing import Optional
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"mesage": "The shortest path between the Avengers start_word and end_word"}
+
+word_dict = ["ironman", "captain america", "hulk","thor", "black panther","dr strange", "falcon", "thanos", "spiderman",
+             "antman", "wasp"]
+
+
+@app.get("/shortest-path")
+async def shortest_path(start_word: str, end_word: str):
+    if start_word == end_word:
+        return 0
+
+    if end_word not in word_dict:
+        return JSONResponse("Please try another Avenger!")
+
+    if start_word not in word_dict:
+        return JSONResponse("Please try another Avenger!")
+
+    # assume total length of the string as min distance
+    short_dist = len(word_dict) + 1
+
+    # traverse through the entire dict
+    for index in range(len(word_dict)):
+
+        if word_dict[index] == start_word:
+            for search in range(len(word_dict)):
+
+                if word_dict[search] == end_word:
+
+                    # the distance between the words is the first word index - the curr word index
+                    curr_wrd = abs(index - search) - 1;
+
+                    # comparing current distance with previous distance
+                    if curr_wrd < short_dist:
+                        short_dist = curr_wrd
+
+    return short_dist
