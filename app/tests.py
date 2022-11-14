b41b33ccb2b96678957ba44542e90b020bf8b3f8
V Thulisile Sibanda <66913810+thulieblack@users.noreply.github.com>
2022-11-14 13:51:41 +0200

    Create tests.py

---
+++ app/tests.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+def test_shortest_path():
+    response = client.get("/shortest-path")
+    assert response.status_code == 200
+    assert response.json() == {"start_word": "apple"}
