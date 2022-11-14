95b34289acab9515dcbf5af2113d8b24600b7f66
V Thulisile Sibanda <66913810+thulieblack@users.noreply.github.com>
2022-11-14 10:33:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from typing import Optional
 from fastapi.responses import JSONResponse
 
 description = """
-### Avengers Assemble API aims at finding the shortest path between two Avenger Hero's or Villain 🚀🚀
+This API takes two words and a dictionary of words as input and return the shortest possible path between the two words.🚀🚀
+
+- Both the start and end words are lowercase strings of varying lengths.
+- The start and end words can be different lengths.
+- Only one letter can be changed, added, or deleted at a time.
+- Each intermediate word must exist in the dictionary of words.
 """
 
 app = FastAPI(
-    title= "Avengers Assemble",
-    version= "1.0",
-    description= description
+    title= "Shortest Path API - OpenAPI 3.0",
+    description= description,
+    license_info= {"name" : "Apache 2.0",
+              "url" : "http://www.apache.org/licenses/LICENSE-2.0.html",
+              "version" : "1.0.0"
+
+  }
+
 
 )
 
-@app.get("/")
-async def root():
-    return {"mesage": "The shortest path between two Avengers"}
+@app.get("/shortest-path",summary= "Returns the shortest possible path between two words"
+    , description= "This API returns the shortest possible path between two words contained inside a dictionary of words.")
+async def shortest_path(word_dict: str = Query(example= "apple pear banana orange grapes"),
+                        start_word: str = Query(example="apple"),
+                        end_word: str= Query(example="orange")):
 
-word_dict = ["ironman", "captain america", "hulk","thor", "black panther","dr strange", "falcon", "thanos", "spiderman",
-             "antman", "wasp"]
-
-
-@app.get("/shortest-path/")
-async def shortest_path(start_word: str, end_word: str):
     if start_word == end_word:
-        return 0
+        return JSONResponse("Try Another Value")
 
     if end_word not in word_dict:
-        return JSONResponse("Please try another Avenger!")
+        return JSONResponse("Invalid Parameter!")
 
     if start_word not in word_dict:
-        return JSONResponse("Please try another Avenger!")
+        return JSONResponse("Invalid Parameter!")
+    
+    # get individual words in a list
+    word_dict = word_dict.split(" ")
 
     # assume total length of the string as min distance
     short_dist = len(word_dict) + 1
